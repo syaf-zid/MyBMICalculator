c37f7102fdd2e4047b08415932a7b2858abfafd1
syafzid <18052270@myrp.edu.sg>
2019-07-05 14:23:11 +0800

    without enhancement

--- app/src/main/java/sg/edu/rp/c346/mybmicalculator/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/mybmicalculator/MainActivity.java
@@ -1,13 +1,93 @@
 package sg.edu.rp.c346.mybmicalculator;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etWeight, etHeight;
+    Button btnCalc, btnReset;
+    TextView tvDate, tvBMI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etWeight = findViewById(R.id.editTextWeight);
+        etHeight = findViewById(R.id.editTextHeight);
+        btnCalc = findViewById(R.id.buttonCalculate);
+        btnReset = findViewById(R.id.buttonReset);
+        tvDate = findViewById(R.id.textViewDate);
+        tvBMI = findViewById(R.id.textViewBMI);
+
+        btnCalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float weight = Float.parseFloat(etWeight.getText().toString());
+                float height = Float.parseFloat(etHeight.getText().toString());
+                float bmi = weight / (height * height);
+
+                String result = String.format("%.2f", bmi);
+
+                Calendar now = Calendar.getInstance();
+                String dateTime = now.get(Calendar.DAY_OF_MONTH) + "/" +
+                        (now.get(Calendar.MONTH)+1) + "/" +
+                        now.get(Calendar.YEAR) + " " +
+                        now.get(Calendar.HOUR_OF_DAY) + ":" +
+                        now.get(Calendar.MINUTE);
+
+                tvDate.setText(dateTime);
+                tvBMI.setText(result);
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etHeight.setText("");
+                etWeight.setText("");
+                tvDate.setText("");
+                tvBMI.setText("");
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        String bmi = tvBMI.getText().toString();
+        String date = tvDate.getText().toString();
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor prefEdit = prefs.edit();
+
+        prefEdit.putString("date", date);
+        prefEdit.putString("bmi", bmi);
+
+
+        prefEdit.commit();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String defaultDate = prefs.getString("date", "");
+        String defaultBMI = prefs.getString("bmi", "");
+
+        tvDate.setText(defaultDate);
+        tvBMI.setText("" + defaultBMI);
     }
 }
